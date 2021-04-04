e63ec8f244d98df0601a46a2bb7b9ae36223402a
Andrés Velasco <velasco.andrs@gmail.com>
2021-04-03 20:27:35 -0500

    education

--- src/app/sections/education/education.module.ts
+++ src/app/sections/education/education.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TimelineModule} from 'primeng/timeline';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import {SharedModule} from 'primeng/api';
+
 
 import { EducationRoutingModule } from './education-routing.module';
 import { EducationRouteComponent } from './routes/education-route/education-route.component';
+import {TRANSLOCO_SCOPE, TranslocoModule} from '@ngneat/transloco';
 
 
 @NgModule({
@@ -11,7 +17,13 @@ import { EducationRouteComponent } from './routes/education-route/education-rout
   ],
   imports: [
     CommonModule,
-    EducationRoutingModule
-  ]
+    EducationRoutingModule,
+    TimelineModule,
+    SharedModule,
+    CardModule,
+    ButtonModule,
+    TranslocoModule,
+  ],
+  providers: [{provide: TRANSLOCO_SCOPE, useValue: 'education'}],
 })
 export class EducationModule { }
--- src/app/sections/education/routes/education-route/education-route.component.ts
+++ src/app/sections/education/routes/education-route/education-route.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PrimeIcons} from 'primeng/api';
+
+interface IEvent {
+  status: string;
+  dates: string;
+  icon: PrimeIcons;
+  color: string;
+  image: string;
+  description: string;
+}
+
 
 @Component({
   selector: 'app-education-route',
@@ -7,9 +18,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationRouteComponent implements OnInit {
 
-  constructor() { }
+  events: IEvent[];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.events = [
+      {
+        status: 'montufar.name',
+        dates: '2008 - 2014',
+        icon: PrimeIcons.BOOK,
+        color: '#ffffff',
+        image: 'mh.png',
+        description: 'montufar.description'
+      },
+      {
+        status: 'poli.name',
+        dates: '2014 - 2021',
+        icon: PrimeIcons.PAPERCLIP,
+        color: '#ffffff',
+        image: 'epn.png',
+        description: 'poli.description'
+      },
+    ];
   }
 
 }
