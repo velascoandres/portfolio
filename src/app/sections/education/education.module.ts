845b9a400e9b24fc515656d73f9da8b0df8538e7
Andrés Velasco <velasco.andrs@gmail.com>
2021-04-04 16:16:11 -0500

    loader images for education

--- src/app/sections/education/education.module.ts
+++ src/app/sections/education/education.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import {TimelineModule} from 'primeng/timeline';
 import {CardModule} from 'primeng/card';
 import {ButtonModule} from 'primeng/button';
-import {SharedModule} from 'primeng/api';
 
 
 import { EducationRoutingModule } from './education-routing.module';
 import { EducationRouteComponent } from './routes/education-route/education-route.component';
 import {TRANSLOCO_SCOPE, TranslocoModule} from '@ngneat/transloco';
+import {SharedModule} from '../../shared/shared.module';
 
 
 @NgModule({
@@ -23,6 +23,7 @@ import {TRANSLOCO_SCOPE, TranslocoModule} from '@ngneat/transloco';
     CardModule,
     ButtonModule,
     TranslocoModule,
+    SharedModule,
   ],
   providers: [{provide: TRANSLOCO_SCOPE, useValue: 'education'}],
 })
