1e6c2fd629c34d828af48727d8bd060582e9862e
Andrés Velasco <velasco.andrs@gmail.com>
2021-05-28 23:17:28 -0500

    official deploy

--- src/app/sections/about-me/routes/about-me-route/about-me-route.component.ts
+++ src/app/sections/about-me/routes/about-me-route/about-me-route.component.ts
@@ -20,17 +20,17 @@ export class AboutMeRouteComponent implements OnInit {
     {
       type: 'github',
       redirectTo: 'https://github.com/velascoandres',
-      iconUrl: '/assets/images/logos/companies/github.svg'
+      iconUrl: 'assets/images/logos/companies/github.svg'
     },
     {
       type: 'linkedin',
       redirectTo: 'https://www.linkedin.com/in/andr%C3%A9s-velasco-210303135/',
-      iconUrl: '/assets/images/logos/companies/linkedin.svg'
+      iconUrl: 'assets/images/logos/companies/linkedin.svg'
     },
     {
       type: 'twitter',
       redirectTo: 'https://twitter.com/Andres33033134',
-      iconUrl: '/assets/images/logos/companies/twitter.svg'
+      iconUrl: 'assets/images/logos/companies/twitter.svg'
     },
   ];
 
